c867986bc160c90afce73394ec8ddbb5c5a1bede
maggie-schmit <mschmit@umail.ucsb.edu>
2019-02-19 19:17:58 -0800

    sem_test stuff

--- sem_test.c
+++ sem_test.c
@@ -10,6 +10,8 @@ void* thread(void* arg)
     //wait
     // sem_wait(&mutex);
     printf("\nEntered..\n");
+    pthread_create(&t2,NULL,thread,NULL);
+    pthread_join(t2, NULL);
 
     //critical section
     sleep(4);
@@ -19,6 +21,13 @@ void* thread(void* arg)
     // sem_post(&mutex);
 }
 
+void* thread2(woid* arg){
+  printf("AHOY THERE\n");
+  sleep(4);
+  printf("AHOY THERE\n");
+  sleep(4);
+}
+
 
 int main()
 {
@@ -26,9 +35,9 @@ int main()
     pthread_t t1,t2;
     pthread_create(&t1,NULL,thread,NULL);
     sleep(2);
-    pthread_create(&t2,NULL,thread,NULL);
-    pthread_join(t1,NULL);
-    pthread_join(t2,NULL);
+
+    // pthread_join(t1,NULL);
+    // pthread_join(t2,NULL);
     // sem_destroy(&mutex);
     return 0;
 }
