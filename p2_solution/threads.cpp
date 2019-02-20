b20cdfded23b4d43c04b8072fcc028ddf35ac2fe
dananguyenucsb <dana.nguyensths@gmail.com>
2019-02-19 17:46:22 -0800

    got semafor test

--- p2_solution/threads.cpp
+++ p2_solution/threads.cpp
@@ -16,11 +16,11 @@
 #include <time.h>
 #include <string.h>
 #include <queue>
-
-
-
-/* 
- * these could go in a .h file but i'm lazy 
+#include <semaphore.h>
+#include <unordered_map>
+#include <map>
+/*
+ * these could go in a .h file but i'm lazy
  * see comments before functions for detail
  */
 void signal_handler(int signo);
@@ -28,8 +28,8 @@ void the_nowhere_zone(void);
 static int ptr_mangle(int p);
 
 
-/* 
- *Timer globals 
+/*
+ *Timer globals
  */
 static struct timeval tv1,tv2;
 static struct itimerval interval_timer = {0}, current_timer = {0}, zero_timer = {0};
@@ -38,7 +38,7 @@ static struct sigaction act;
 
 
 /*
- * Timer macros for more precise time control 
+ * Timer macros for more precise time control
  */
 
 #define PAUSE_TIMER setitimer(ITIMER_REAL,&zero_timer,&current_timer)
@@ -47,10 +47,10 @@ static struct sigaction act;
 #define STOP_TIMER setitimer(ITIMER_REAL,&zero_timer,NULL)
 /* number of ms for timer */
 #define INTERVAL 50
-
+#define SEM_VALUE_MAX 65536
 
 /*
- * Thread Control Block definition 
+ * Thread Control Block definition
  */
 typedef struct {
 	/* pthread_t usually typedef as unsigned long int */
@@ -58,24 +58,48 @@ typedef struct {
 	/* jmp_buf usually defined as struct with __jmpbuf internal buffer
 	   which holds the 6 registers for saving and restoring state */
 	jmp_buf jb;
-	/* stack pointer for thread; for main thread, this will be NULL */	
+	/* stack pointer for thread; for main thread, this will be NULL */
 	char *stack;
+
+	bool blocked = false;
 } tcb_t;
 
-
-
-/* 
- * Globals for thread scheduling and control 
+/*
+ * Additional Semaphore Struct definition
  */
+typedef struct {
+	// sem_t *mysem;
+	unsigned int sem_id;
+	//stores the current value
+	unsigned cur_val;
+	//a pointer to a queue for threads that are waiting
+	int* thread_queue_ptr;
+
+	/*queue for threads that are waiting*/
+	std::queue<tcb_t> wait_pool;
+	//a flag that indicates whether the semaphore is initialized
+	bool flag_init = false;
+} mysem_t;
+
+/*
+ * Globals for thread scheduling and control
+ */
+
+//keep track of semaphore
+std::unordered_map<unsigned int, sem_t*> semaphore_map;
 
 /* queue for pool thread, easy for round robin */
 static std::queue<tcb_t> thread_pool;
+
+/*queue for threads that are waiting*/
+std::queue<tcb_t> wait_pool;
+
 /* keep separate handle for main thread */
 static tcb_t main_tcb;
 static tcb_t garbage_collector;
 
 /* for assigning id to threads; main implicitly has 0 */
-static unsigned long id_counter = 1; 
+static unsigned long id_counter = 1;
 /* we initialize in pthread_create only once */
 static int has_initialized = 0;
 
@@ -86,7 +110,7 @@ static int has_initialized = 0;
  * init()
  *
  * Initialize thread subsystem and scheduler
- * only called once, when first initializing timer/thread subsystem, etc... 
+ * only called once, when first initializing timer/thread subsystem, etc...
  */
 void init() {
 	/* on signal, call signal_handler function */
@@ -113,7 +137,7 @@ void init() {
 	/* create thread control buffer for main thread, set as current active tcb */
 	main_tcb.id = 0;
 	main_tcb.stack = NULL;
-	
+
 	/* front of thread_pool is the active thread */
 	thread_pool.push(main_tcb);
 
@@ -132,21 +156,21 @@ void init() {
 
 	/* Initialize timer and wait for first sigalarm to go off */
 	START_TIMER;
-	pause();	
+	pause();
 }
 
 
 
-/* 
+/*
  * pthread_create()
- * 
+ *
  * create a new thread and return 0 if successful.
  * also initializes thread subsystem & scheduler on
- * first invocation 
+ * first invocation
  */
 int pthread_create(pthread_t *restrict_thread, const pthread_attr_t *restrict_attr,
                    void *(*start_routine)(void*), void *restrict_arg) {
-	
+
 	/* set up thread subsystem and timer */
 	if(!has_initialized) {
 		has_initialized = 1;
@@ -157,7 +181,7 @@ int pthread_create(pthread_t *restrict_thread, const pthread_attr_t *restrict_at
     PAUSE_TIMER;
 
 	/* create thread control block for new thread
-	   restrict_thread is basically the thread id 
+	   restrict_thread is basically the thread id
 	   which main will have access to */
 	tcb_t tmp_tcb;
 	tmp_tcb.id = id_counter++;
@@ -171,12 +195,12 @@ int pthread_create(pthread_t *restrict_thread, const pthread_attr_t *restrict_at
 
 	*(int*)(tmp_tcb.stack+32763) = (int)restrict_arg;
 	*(int*)(tmp_tcb.stack+32759) = (int)pthread_exit;
-	
+
 	/* initialize jump buf structure to be 0, just in case there's garbage */
 	memset(&tmp_tcb.jb,0,sizeof(tmp_tcb.jb));
 	/* the jmp buffer has a stored signal mask; zero it out just in case */
 	sigemptyset(&tmp_tcb.jb->__saved_mask);
-	
+
 	/* modify the stack pointer and instruction pointer for this thread's
 	   jmp buffer. don't forget to mangle! */
 	tmp_tcb.jb->__jmpbuf[4] = ptr_mangle((uintptr_t)(tmp_tcb.stack+32759));
@@ -184,21 +208,21 @@ int pthread_create(pthread_t *restrict_thread, const pthread_attr_t *restrict_at
 
 	/* new thread is ready to be scheduled! */
 	thread_pool.push(tmp_tcb);
-    
+
     /* resume timer */
     RESUME_TIMER;
 
-    return 0;	
+    return 0;
 }
 
 
 
-/* 
+/*
  * pthread_self()
  *
  * just return the current thread's id
  * undefined if thread has not yet been created
- * (e.g., main thread before setting up thread subsystem) 
+ * (e.g., main thread before setting up thread subsystem)
  */
 pthread_t pthread_self(void) {
 	if(thread_pool.size() == 0) {
@@ -210,11 +234,11 @@ pthread_t pthread_self(void) {
 
 
 
-/* 
+/*
  * pthread_exit()
  *
  * pthread_exit gets returned to from start_routine
- * here, we should clean up thread (and exit if no more threads) 
+ * here, we should clean up thread (and exit if no more threads)
  */
 void pthread_exit(void *value_ptr) {
 
@@ -235,20 +259,133 @@ void pthread_exit(void *value_ptr) {
 			   We'll free anyways, for completeness */
 			free((void*) garbage_collector.stack);
 			exit(0);
-		} 
+		}
 	}
 
 	/* Jump to garbage collector stack frame to free memory and scheduler another thread.
 	   Since we're currently "living" on this thread's stack frame, deleting it while we're
 	   on it would be undefined behavior */
-	longjmp(garbage_collector.jb,1); 
+	longjmp(garbage_collector.jb,1);
 }
 
+int pthread_join(pthread_t thread, void **value_ptr){
+	// set that this pthread is blocked
+	thread_pool.front().blocked = true;
 
-/* 
+	// TODO: check if thread is exited already
+	
+
+	return 0;
+}
+
+//TODO: sem_init, sem_destroy, sem_wait, sem_post
+//this is useful: https://os.itec.kit.edu/downloads/sysarch09-mutualexclusionADD.pdf
+
+//global to declare current semaphore??
+
+int sem_init (sem_t ∗sem, int pshared, unsigned value ){
+	
+	unsigned long sem_id_count = 0;
+
+	mysem_t cur_sem;
+	cur_sem.sem_id = sem_id_count;
+	sem_id_count++;
+
+	// cur_sem.mysem = *sem;
+	if (value < SEM_VALUE_MAX){
+		cur_sem.cur_val = value;
+	} else {
+		//return error bc value should be less than sem value max
+		return -1;
+	}
+
+	if (pshared != 0){
+		//return error bc pshared should always be 0
+		return -1;
+	}
+
+	cur_sem.flag_init = true;
+	sem->__align = &cur_sem;
+	// *sem = tmp_sem.mysem;
+	semaphore_map[cur_sem.sem_id] = cur_sem;
+
+	return 0;
+}
+
+int sem_destroy(sem_t *sem){
+
+	return 0;
+}
+
+//idk need to call block whatever
+int sem_wait(sem_t *sem){
+	mysem_t cur_sem;
+	
+	for (semaphore_map::const_iterator it = map.begin(); it != map.end(); ++it) {
+  		if ((it->second) == (sem->__align)){
+  			cur_sem = it->second;
+  		}
+  			// return it->first;
+  		else{
+  			printf("shit semaphore (wait) not in the map???\n");
+  		}
+	} 
+
+
+	if(cur_sem.cur_val > 0){
+		cur_sem.cur_val = cur_sem.cur_val - 1;
+		return 0;
+	} else if (cur_sem.cur_val < 0){
+		return -1;
+	}
+
+	if (cur_sem.cur_val == 0){
+		//not sure if correct....
+		// thread_pool.front().blocked = true;
+		(cur_sem.wait_pool).push(thread_pool.front());
+	}
+
+	return 0;
+
+}
+
+int sem_post(sem_t *sem){
+	mysem_t cur_sem;
+	
+	for (semaphore_map::const_iterator it = map.begin(); it != map.end(); ++it) {
+  		if ((it->second).mysem == (sem->__align)){
+  			cur_sem = it->second;
+  		}
+  			// return it->first;
+  		else{
+  			printf("shit semaphore (post) not in the map???\n");
+  		}
+	} 
+
+	cur_sem.cur_val = cur_sem.cur_val + 1;
+	if (cur_sem.cur_val > 0){
+		(cur_sem.wait_pool).pop();
+		thread_pool.push((cur_sem.wait_pool).front())
+	} else if (cur_sem.cur_val < 0){
+		return -1;
+	}
+
+
+	return 0;
+}
+
+void lock(){
+	// we don't want to be interrupted
+	STOP_TIMER;
+}
+
+void unlock(){
+	RESUME_TIMER;
+}
+/*
  * signal_handler()
- * 
- * called when SIGALRM goes off from timer 
+ *
+ * called when SIGALRM goes off from timer
  */
 void signal_handler(int signo) {
 
@@ -256,7 +393,7 @@ void signal_handler(int signo) {
 	if(thread_pool.size() <= 1) {
 		return;
 	}
-	
+
 	/* Time to schedule another thread! Use setjmp to save this thread's context
 	   on direct invocation, setjmp returns 0. if jumped to from longjmp, returns
 	   non-zero value. */
@@ -265,6 +402,12 @@ void signal_handler(int signo) {
 		thread_pool.push(thread_pool.front());
 		thread_pool.pop();
 		/* resume scheduler and GOOOOOOOOOO */
+		// check if the front thread is blocked.
+		// If it IS blocked, then we want to push it to the back and call another thread
+		while(thread_pool.front().blocked == true){
+			thread_pool.push(thread_pool.front());
+			thread_pool.pop();
+		}
 		longjmp(thread_pool.front().jb,1);
 	}
 
@@ -273,16 +416,16 @@ void signal_handler(int signo) {
 }
 
 
-/* 
+/*
  * the_nowhere_zone()
- * 
+ *
  * used as a temporary holding space to safely clean up threads.
  * also acts as a pseudo-scheduler by scheduling the next thread manually
  */
 void the_nowhere_zone(void) {
-	/* free stack memory of exiting thread 
+	/* free stack memory of exiting thread
 	   Note: if this is main thread, we're OK since
-	   free(NULL) works */ 
+	   free(NULL) works */
 	free((void*) thread_pool.front().stack);
 	thread_pool.front().stack = NULL;
 
@@ -299,10 +442,12 @@ void the_nowhere_zone(void) {
 	}
 }
 
-/* 
+
+
+/*
  * ptr_mangle()
  *
- * ptr mangle magic; for security reasons 
+ * ptr mangle magic; for security reasons
  */
 int ptr_mangle(int p)
 {
@@ -317,4 +462,3 @@ int ptr_mangle(int p)
     );
     return ret;
 }
-
